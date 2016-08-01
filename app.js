90293482ad2310938bf5484167626d2debe7fb3e
Lance Marks <marksla@oregonstate.edu>
2016-08-01 18:39:51 -0400

    debugging mysql connection

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var handlebars = require("express-handlebars").create({defaultLayout:'main'});
 var mysql = require('mysql');
 var pool = mysql.createPool({
   host: 'localhost',
-  user: 'marksladbaws',
-  password: 'AppleGoatRainCow',
-  database: 'marksladbaws'
+  user: 'student',
+  password: 'default',
+  database: 'student'
 });
 var session = require('express-session');
 var bodyparser = require('body-parser');  //load body parser for POST
